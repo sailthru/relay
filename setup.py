f8a0f64b51942412d442e8359d69c744a6b7557d
Alex Gaudio <adgaudio@gmail.com>
2014-10-06 19:10:30 -0400

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+try:
+    from setuptools import find_packages
+except ImportError:
+    print ("Please install Distutils and setuptools"
+           " before installing this package")
+    raise
+
+setup(
+    name='Relay',
+    version='0.0.1',
+    description=(
+        'Run an arbitrary bash command as a mesos framework.'
+        ' Scale up number of concurrently running instances based on a metric.'
+        ' Generally good for auto-scaling workers.  Very similar to Marathon,'
+        ' but designed for applications that quit or fail often.'
+    ),
+    long_description="Check the project homepage for details",
+    keywords=['mesos', 'marathon', 'relay', 'framework'],
+
+    author='Alex Gaudio',
+    author_email='adgaudio@gmail.com',
+    url='http://github.com/sailthru/relay',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = [
+        'mesos', 'mesos.native', 'mesos.cli', 'mesos.interface'],
+    tests_require=['nose'],
+    test_suite="nose.main",
+)
