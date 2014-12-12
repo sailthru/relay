a40dfd2bb6496e9150a893e4a51d2185eee7e447
Alex Gaudio <adgaudio@gmail.com>
2014-12-12 16:24:17 -0500

    Back to development: 0.1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='relay.runner',
-    version='0.1.7',
+    version='0.1.8.dev0',
     description=(
         'A smart thermostat.  Given a metric, or some timeseries that should'
         ' approach a given target, add heat or coolant as necessary'
